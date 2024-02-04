6fae7cc97bb0257f999523ae6638358fb8a27768
Blackstars <nelson.64@live.fr>
2024-02-04 16:39:27 +0100

    Add authentication controllers and services

--- backend/src/router.js
+++ backend/src/router.js
@@ -7,16 +7,13 @@ const router = express.Router();
 /* ************************************************************************* */
 
 // Import itemControllers module for handling item-related operations
-const itemControllers = require("./controllers/itemControllers");
+const authControllers = require("./controllers/authControllers");
 
-// Route to get a list of items
-router.get("/items", itemControllers.browse);
+// Define the API routes
 
-// Route to get a specific item by ID
-router.get("/items/:id", itemControllers.read);
-
-// Route to add a new item
-router.post("/items", itemControllers.add);
+/* **************************** AUTH ****************************************** */
+// Post
+router.post("/login", authControllers.login);
 
 /* ************************************************************************* */
 
