30b3f908b7040b957581d6f7cba2e3dabd1b541a
Blackstars <nelson.64@live.fr>
2024-02-11 19:03:08 +0100

    Add footer links and images

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,33 @@
+import { Link } from "react-router-dom";
+import mafumafu from "../assets/mafumafu-mini.png";
+import linkedin from "../assets/linkedin.png";
+import github from "../assets/github.png";
+
 function Footer() {
-  return <p>Footer</p>;
+  return (
+    <footer className="footer">
+      <Link className="footer-link-portfolio" to="https://nelson-almeida.fr/">
+        <img className="footer-img-portfolio" src={mafumafu} alt="mafumafu" />
+        <p className="footer-p-portfolio">Create by: Blackstars</p>
+      </Link>
+      <div className="footer-link">
+        <Link
+          className="footer-link-select"
+          to="https://www.linkedin.com/in/almeidanelson64/"
+        >
+          <img className="footer-link-img" src={linkedin} alt="linkedin" />
+          <p className="footer-link-p">Linkedin</p>
+        </Link>
+        <Link
+          className="footer-link-select"
+          to="https://github.com/blackstars64"
+        >
+          <img className="footer-link-img" src={github} alt="" />
+          <p className="footer-link-p">GitHub</p>
+        </Link>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
