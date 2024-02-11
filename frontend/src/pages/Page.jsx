04e7aa2addf533e573c094b0622dc1e29b7d0fb6
Blackstars <nelson.64@live.fr>
2024-02-11 18:32:47 +0100

    Add Footer and Nav components, update App.jsx and Page.jsx

--- frontend/src/pages/Page.jsx
+++ frontend/src/pages/Page.jsx
@@ -1,14 +1,38 @@
 import { Outlet } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import isEmty from "../utils/isEmpty";
+import Nav from "../components/Nav";
+import Footer from "../components/Footer";
+import { getOneUser } from "../actions/user.action";
 
 function Page() {
+  const dispatch = useDispatch();
+  const token = sessionStorage.getItem("token");
+  const user = useSelector((state) => state.userReducer);
+
+  useEffect(() => {
+    dispatch(getOneUser(token));
+  }, [dispatch, token]);
   return (
     <>
       <header>
-        <h2>Page</h2>
+        <div>
+          <picture>
+            <img src="" alt="" />
+          </picture>
+          <div>
+            <p>{!isEmty(user) ? user.username : "No Connected"}</p>
+            <p>DP: {!isEmty(user) ? user.digi_point : "0"}</p>
+          </div>
+        </div>
+
+        <Nav />
       </header>
       <main>
         <Outlet />
       </main>
+      <Footer />
     </>
   );
 }
