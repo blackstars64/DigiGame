7a7cc47cdab9806fe0e37624c6fb1d4df1b40fcb
Blackstars <nelson.64@live.fr>
2024-03-15 00:35:14 +0100

    Refactor DeleteDigimon component to use prop types

--- frontend/src/components/popups/DeleteDigimon.jsx
+++ frontend/src/components/popups/DeleteDigimon.jsx
@@ -1,5 +1,46 @@
-function DeleteDigimon() {
-  return <p>DeleteDigimon</p>;
+import propTypes from "prop-types";
+
+function DeleteDigimon({
+  setIdBeingEdited,
+  setIsDelete,
+  dispatch,
+  deleteDigimon,
+  digimon,
+}) {
+  return (
+    <section>
+      <p>Are you sure you want to delete this digimon?</p>
+      <button
+        onClick={() => {
+          setIsDelete(false);
+          setIdBeingEdited(null);
+        }}
+        type="button"
+      >
+        No
+      </button>
+      <button
+        onClick={() => {
+          setIsDelete(false);
+          dispatch(deleteDigimon(digimon.id));
+          setIdBeingEdited(null);
+        }}
+        type="button"
+      >
+        Yes
+      </button>
+    </section>
+  );
 }
+
+DeleteDigimon.propTypes = {
+  setIsDelete: propTypes.func.isRequired,
+  dispatch: propTypes.func.isRequired,
+  deleteDigimon: propTypes.func.isRequired,
+  setIdBeingEdited: propTypes.func.isRequired,
+  digimon: propTypes.shape({
+    id: propTypes.number.isRequired,
+  }).isRequired,
+};
 
 export default DeleteDigimon;
