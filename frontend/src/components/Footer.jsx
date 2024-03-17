bde9f83a284f73a59b360b00bec97e18a66364a7
Blackstars <nelson.64@live.fr>
2024-03-17 01:21:51 +0100

    Add Footer and Page styles

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,12 +2,15 @@ import { Link } from "react-router-dom";
 import mafumafu from "../assets/mafumafu-mini.png";
 import linkedin from "../assets/linkedin.png";
 import github from "../assets/github.png";
+import "../scss/Footer.scss";
 
 function Footer() {
   return (
     <footer className="footer">
       <Link className="footer-link-portfolio" to="https://nelson-almeida.fr/">
-        <img className="footer-img-portfolio" src={mafumafu} alt="mafumafu" />
+        <picture>
+          <img className="footer-img-portfolio" src={mafumafu} alt="mafumafu" />
+        </picture>
         <p className="footer-p-portfolio">Create by: Blackstars</p>
       </Link>
       <div className="footer-link">
@@ -15,14 +18,18 @@ function Footer() {
           className="footer-link-select"
           to="https://www.linkedin.com/in/almeidanelson64/"
         >
-          <img className="footer-link-img" src={linkedin} alt="linkedin" />
+          <picture>
+            <img className="footer-link-img" src={linkedin} alt="linkedin" />
+          </picture>
           <p className="footer-link-p">Linkedin</p>
         </Link>
         <Link
           className="footer-link-select"
           to="https://github.com/blackstars64"
         >
-          <img className="footer-link-img" src={github} alt="" />
+          <picture>
+            <img className="footer-link-img" src={github} alt="" />
+          </picture>
           <p className="footer-link-p">GitHub</p>
         </Link>
       </div>
