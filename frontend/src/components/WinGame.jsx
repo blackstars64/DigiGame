b2fe550d9796cd77533ef55b14e950bf9d4a7e37
Blackstars <nelson.64@live.fr>
2024-03-19 02:08:38 +0100

    Refactor WinGame component to include PropTypes

--- frontend/src/components/WinGame.jsx
+++ frontend/src/components/WinGame.jsx
@@ -1,5 +1,39 @@
-function WinGame() {
-  return <p>WinGame</p>;
+import PropTypes from "prop-types";
+import DigimonCard from "./DigimonCard";
+
+function WinGame({ digimon, digiPoints, setDigiPoints, clickRestart }) {
+  return (
+    <>
+      <h2 className="h1">ScratchDigimon</h2>
+      <div className="wg-c">
+        <h3>You Win</h3>
+        <DigimonCard digimon={digimon} />
+      </div>
+      <p className="wg-dp">and {digiPoints}DP</p>
+      <button
+        className="btn-violet"
+        onClick={() => {
+          setDigiPoints(400);
+          clickRestart();
+        }}
+        type="button"
+      >
+        Continue
+      </button>
+    </>
+  );
 }
+
+WinGame.propTypes = {
+  digimon: PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    level: PropTypes.string,
+    img: PropTypes.string,
+  }).isRequired,
+  digiPoints: PropTypes.number.isRequired,
+  setDigiPoints: PropTypes.func.isRequired,
+  clickRestart: PropTypes.func.isRequired,
+};
 
 export default WinGame;
