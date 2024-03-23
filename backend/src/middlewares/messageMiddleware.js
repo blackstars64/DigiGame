2765f765de950fd71cc25be9826169ec7edb53bc
Blackstars <nelson.64@live.fr>
2024-03-23 14:27:00 +0100

    Refactor messageControllers and messageMiddleware

--- backend/src/middlewares/messageMiddleware.js
+++ backend/src/middlewares/messageMiddleware.js
@@ -1,16 +1,15 @@
 const joi = require("joi");
 
 const MessageSchema = joi.object({
-  title: joi.string().alphanum().min(3).max(30).required(),
   message: joi.string().alphanum().min(15).max(500).required(),
   userId: joi.number().required(),
   received: joi.date().required(),
 });
 
 const messageMiddleware = (req, res, next) => {
-  const { title, message, userId, received } = req.body;
+  const { message, userId, received } = req.body;
   const { error } = MessageSchema.validate(
-    { title, message, userId, received },
+    { message, userId, received },
     { abortEarly: false }
   );
 
